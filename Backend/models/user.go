7c611682ea3562106d06541f15cbd9f36522a5ac
Kevin Leonardi <kevinleonardi@yahoo.com>
2022-06-12 10:44:52 +0700

    starting over

--- Backend/models/user.go
+++
@@ -1,9 +0,0 @@
-package models
-
-// ini adalah struct representasi dari table user pada database
-type User struct {
-	Id       int    `json:"id" gorm:"primary-key"`
-	Name     string `json:"name"`
-	Email    string `json:"email" gorm:"unique"`
-	Password []byte `json:"-"` // karena hasil encrypt password harus dalam bentuk byte
-}
