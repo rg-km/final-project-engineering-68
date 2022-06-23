2546edc38099013418b271a86e57a8dbfee70752
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-06-23 17:03:38 +0700

    blogs sudahh jadii

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,19 +8,15 @@ import NavbarComp from "./components/navbar/NavbarComp";
 import { BrowserRouter, Link, Route, Routes, Switch } from "react-router-dom";
 import AboutUs from "./pages/AboutUs";
 import Category from "./pages/Category";
+import Blogs from "./components/Blogs/Blogs";
 
 function App() {
   return (
     <div className="App">
       <NavbarComp />
 
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/category" element={<Category />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <Blogs />
+      
     </div>
   );
 }
