3c6818fee3704ae97b32536daa4b2048ae6ffac6
faniadifrihandoko <faniadifrihandoko1@gmail.com>
2022-06-26 12:51:00 +0700

    coba

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from "react";
-import Axios from "axios";
-import CardsUI from "./CardsUI";
+import axios from "axios";
+import React from "react";
+import CardUI from "./CardsUI";
 // import axios from "axios";
 
 export default function Card() {
-  const [datak, setDatak] = useState(0);
-
-  const getData = async () => {
-    try {
-      const res = await Axios.get(
-        "http://localhost:8082/api/konten?id_kategori=500001"
-      );
-      setDatak(res.data);
-    } catch (error) {
-      alert(error.message);
-    }
+  const [card, setCard] = React.useState([]);
+  const getCard = async () => {
+    const res = await axios.get("http://localhost:8082/api/kategori");
+    setCard(res.data.data);
   };
 
-  useEffect(() => {
-    getData();
-  });
+  // const listCard = card.map((item, index) => {
+  //   return (
+  //     <Card
+  //       key={index}
+  //       image={item.image}
+  //       title={item.title}
+  //       description={item.description}
+  //     />
+  //   );
+  // });
 
   return (
     <div>
@@ -27,11 +27,7 @@ export default function Card() {
         <h4 class="mb-5 text-center">Apa yang baru dari di Nakama?</h4>
         <div class="container">
           <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-            {datak.map((pr) => (
-              <div class="col-lg-4 mb-3 d-flex align-items-stretch">
-                <CardsUI judul={pr.judul_konten} gambar={pr.src} />
-              </div>
-            ))}
+            <CardUI />y
           </div>
         </div>
       </div>
